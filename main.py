c325f17381481b964c0bf5f86bd6d67eab927da8
pastamancr <ogifull9@gmail.com>
2026-02-26 16:45:44 +0100

    Added a player sprite

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
     interal_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         log_state()
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
         pygame.display.flip()
         dt = interal_clock.tick(60) / 1000
-        print(dt)
+
+
 if __name__ == "__main__":
     main()
