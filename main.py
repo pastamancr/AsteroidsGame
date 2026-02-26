df3703ce94119b2dbbd10db9cbe68f7c19fa21de
pastamancr <ogifull9@gmail.com>
2026-02-26 10:55:13 +0100

    fixed some things... its my first time okay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import pygame
-<<<<<<< HEAD
-from constants.py import SCREEN_WIDTH, SCREEN_HEIGHT
-=======
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
->>>>>>> d6f3c37 (added constants.py and added example print statements in the main.py file)
 
 
 def main():
